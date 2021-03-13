57dd3242cc706a48a5df5a522014219049fc8bc5
gulsahozlu <gulsahozluu@gmail.com>
2021-03-13 13:18:13 +0300

    yeni kod

--- first_app.py
+++ first_app.py
@@ -1,1 +1,18 @@
 print('Hello World')
+
+maas_ahmet = 5000
+maas_ayse  = 7000
+vergi      =  0.27
+
+print(maas_ahmet - (maas_ahmet*vergi))
+print(maas_ayse - (maas_ayse*vergi))
+
+# değişken tanımlama kuralları
+x = 1           #int
+y= 2.3          #float
+name ="cinar"   #string
+isstudent= True #bool
+
+sayi1 ,sayi2 ,isim ,isogrenci  = (5, 8.7 , "gulsah", False)
+
+
