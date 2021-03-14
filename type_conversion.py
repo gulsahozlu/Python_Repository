e631a7570fa78c1ecd755db91a7f35725cd3ec10
gulsahozlu <gulsahozluu@gmail.com>
2021-03-14 13:23:17 +0300

    gönder gelsin

---
+++ type_conversion.py
@@ -0,0 +1,43 @@
+"""
+x = input ('1. sayi:') #input metodu ile kullanıcı sayıgirecek
+y = input ('2. sayi:')
+
+print(type(x))#x ve y'nin tipini yazdır
+print(type(y))
+ 
+toplam = int(x) + int(y)  #x ve y'nin tipini değiştir
+print(toplam)
+
+"""
+
+x = 5
+y = 5.9
+name = 'ÇINAR'
+isOnline = True
+
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(isOnline))
+
+
+x = float(x)
+print(x)
+print(type(x))
+
+y = int(y)
+print(y)
+print(type(y))
+
+result = x + y
+print (result)
+print(type(result))
+
+# isOnline =str(isOnline)
+# print(isOnline)
+# print(type(isOnline))
+
+isOnline = int(isOnline)
+print(isOnline)
+print(type(isOnline))
+
