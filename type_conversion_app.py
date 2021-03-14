cddaa4b7ffc95b9f977ab677e1c4364a995937ee
gulsahozlu <gulsahozluu@gmail.com>
2021-03-14 14:12:48 +0300

    hello

---
+++ type_conversion_app.py
@@ -0,0 +1,15 @@
+
+# Dairenin Alanı : pi*r*r
+# Dairenin Çevresi : 2*pi*r
+# r=3.14 alınız
+
+daireYaricap =float(input('Dairenin Yarıçapını Giriniz:'))
+pi = 3.14
+
+alan = pi *(daireYaricap**2)
+cevre = 2*pi*daireYaricap
+
+print('Dairenin Alanı: ', alan)
+
+print('Dairenin Çevresi:', cevre)
+
